b1e77bab80b00ccd5b873fe93cef6b25b19f2366
Prashant-mahajan <prashantmahajan70@gmail.com>
2017-06-09 23:59:30 +0530

    Histogram of data

---
+++ 3-dogs.py
@@ -0,0 +1,12 @@
+#representation of data using histogram
+import numpy as np
+import matplotlib.pyplot as plt
+
+greyhounds = 500
+labs = 500
+
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
+
+plt.hist([grey_height, lab_height], stacked = True, edgecolor='black', color = ['r', 'b'])
+plt.show()
